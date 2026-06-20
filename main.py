eb5bcd0db0be55521ace5df7ebfee67e1f93850b
aoudy1213-yoyo <aoudy1213@gmail.com>
2026-06-21 02:14:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,26 @@
-import os
-from flask import Flask, request, abort
-from linebot.v3 import WebhookHandler
-from linebot.v3.exceptions import InvalidSignatureError
-from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
-from linebot.v3.webhooks import MessageEvent, TextMessageContent
-
-app = Flask(__name__)
-
-# 設定
-configuration = Configuration(access_token=os.environ.get("CHANNEL_ACCESS_TOKEN"))
-handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
-    body = request.get_data(as_text=True)
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
     with ApiClient(configuration) as api_client:
         line_bot_api = MessagingApi(api_client)
         
+        # 接收到的訊息內容
         msg = event.message.text
-        reply_text = "收到訊息：" + msg
         
-        if msg == "植物狀態":
-            reply_text = "目前植物狀態：濕度 65%，正常！"
-        elif msg == "澆水":
-            reply_text = "好的，正在為您澆水..."
+        # 強制輸出到 Log，方便你隨時確認
+        print(f"收到訊息: {msg}")
+        
+        # 決定要回覆什麼
+        if "植物狀態" in msg:
+            reply_text = "目前植物狀態：濕度 65%，狀況良好！"
+        elif "澆水" in msg:
+            reply_text = "收到指令！系統正在執行澆水程序..."
+        else:
+            reply_text = f"機器人已連線！你剛剛說的是：{msg}"
             
+        # 回覆訊息
         line_bot_api.reply_message(
             ReplyMessageRequest(
                 reply_token=event.reply_token,
                 messages=[TextMessage(text=reply_text)]
             )
         )
-
-if __name__ == "__main__":
-    app.run()
